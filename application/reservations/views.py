1cf19ec546f5f4f701abd6dc0f7d070c4ac08b04
Selina <selinale@cs.helsinki.fi>
2018-04-01 01:34:38 +0300

    kirjautuminen yms. lisäykset

--- application/reservations/views.py
+++ application/reservations/views.py
@@ -1,29 +1,41 @@
-from application import app, db
-from flask import redirect, render_template, request, url_for
-from application.reservations.models import Reservation
+from flask import render_template, request, redirect, url_for
+from flask_login import login_required, current_user
 
-@app.route("/reservations", methods=["GET"])
+from application import app, db
+from application.reservations.models import Reservation
+from application.reservations.forms import ReservationForm
+
+@app.route("/reservations/", methods=["GET"])
 def reservations_index():
     return render_template("reservations/list.html", reservations = Reservation.query.all())
 
 @app.route("/reservations/new/")
+@login_required
 def reservations_form():
-    return render_template("reservations/new.html")
+    return render_template("reservations/new.html", form = ReservationForm())
 
 @app.route("/reservations/<reservation_id>/", methods=["POST"])
+@login_required
 def reservations_set_done(reservation_id):
 
-    t = Reservation.query.get(reservation_id)
-    t.done = True
+    r = Reservation.query.get(reservation_id)
+    r.done = True
     db.session().commit()
   
     return redirect(url_for("reservations_index"))    
 
 @app.route("/reservations/", methods=["POST"])
+@login_required
 def reservations_create():
-    t = Reservation(request.form.get("name"))
+    form = ReservationForm(request.form)
 
-    db.session().add(t)
+    if not form.validate():
+        return render_template("reservations/new.html", form = form)
+
+    r = Reservation(form.name.data)
+    r.done = form.done.data
+
+    db.session().add(r)
     db.session().commit()
   
     return redirect(url_for("reservations_index"))
