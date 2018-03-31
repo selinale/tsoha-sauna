e4f82050c6f98ef6d7f21b8c7755cbc366d24b6c
Selina <selinale@cs.helsinki.fi>
2018-04-01 02:49:10 +0300

    testi

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reservations.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:    
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reservations.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -21,7 +27,7 @@ app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
 login_manager = LoginManager()
-login_manager.setup_app(app)
+login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
@@ -30,4 +36,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    print("ei onnistu")
