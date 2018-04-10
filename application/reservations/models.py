93f628aee25eca5130b1fce219e835744d7981db
Selina <selinale@cs.helsinki.fi>
2018-04-10 15:05:26 +0300

    modelsien muokkausta

--- application/reservations/models.py
+++ application/reservations/models.py
@@ -1,10 +1,7 @@
 from application import db
+from application.models import Base
 
-class Reservation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Reservation(Base):
 
     name = db.Column(db.String(144), nullable=False)
     done = db.Column(db.Boolean, nullable=False)
