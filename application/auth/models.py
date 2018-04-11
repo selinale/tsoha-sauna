2feeede3e91f1b9870dc6fdaad066506387ebccc
Selina <selinale@cs.helsinki.fi>
2018-04-11 11:13:43 +0300

    ulkoasun muokkaamista

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,10 +1,12 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class User(Base):
 
     __tablename__ = "account"
-    
+
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
@@ -25,3 +27,19 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def find_users_with_no_reservations(done=0):
+        stmt = text("SELECT Account.id, Account.name FROM Account"
+                    " LEFT JOIN Reservation ON Reservation.account_id = Account.id"
+                    " WHERE (Reservation.done IS null OR Reservation.done = 1)"
+                    " GROUP BY Account.id"
+                    " HAVING COUNT(Reservation.id) = 0")
+        res = db.engine.execute(stmt)
+
+        response = []
+
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})   
+
+        return response            
