23105e5d0ac4fc2aa8c30127e3b739c80862ba09
Selina <selinale@cs.helsinki.fi>
2018-05-04 00:10:33 +0300

    korjtatu

--- application/reservations/models.py
+++ application/reservations/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from sqlalchemy.sql import text
 
@@ -50,7 +50,7 @@ class Reservation(Base):
 
 
         for row in res:
-            date = row[2] if isinstance(row[2], datetime) else datetime.strptime(row[2], '%Y-%m-%d')
+            date = row[2] if isinstance(row[2], date) else datetime.strptime(row[2], '%Y-%m-%d')
 
             response.append({"household":row[0], "hour":row[1], "date": date})
 
